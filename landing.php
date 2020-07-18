330a8bdfa3c103b4d7d9f1bf49f5e051a11a4904
pizzaboxer <41478239+pizzaboxer@users.noreply.github.com>
2020-07-18 12:03:42 +0100

    add register link and space out blog post listings

--- landing.php
+++ landing.php
@@ -14,19 +14,18 @@
         ?>
         <div class="container">
             <div class="left">
-                <div class="topBarWithItemsThing">
-                    <a href="blogs.php">Blogs</a> &nbsp;<a href="groups.php">Groups</a> &nbsp;<a href="register.php">Register</a> &nbsp;<a href="login.php">Login</a>
-                </div>
+                <?php if(!isset($_SESSION['user'])){ ?>
                 <div class="info">
                     Log in
                 </div>
                 <br>
-                <form method="post" action="/login.php">
+                <form method="post" action="/login">
                     <input required placeholder="Username" type="text" name="username"><br>
                     <input required placeholder="Password" type="password" name="password"><br><br>
-                    <input type="submit" value="Login">
+                    <input type="submit" value="Login"><span><small>   New user? <a href="/register">Register</a></small></span>
                 </form>
                 <br>
+                <?php } ?>
                 <div class="info">
                     Latest Blog Posts
                 </div>
@@ -35,9 +34,16 @@
                     $result = $conn->query("SELECT id, title, author FROM blogs LIMIT 5");
                     while($row = $result->fetch_assoc()) 
                     {
-                        echo "<a href='/viewblog.php?id=".$row['id']."'>".$row['title']."</a> - by <a href='/profile?id=".getID($row['author'], $conn)."'>".$row['author']."</a><br>";
+                        echo "<a href='/viewblog?id=".$row['id']."'>".$row['title']."</a> - by <a href='/profile?id=".getID($row['author'], $conn)."'>".$row['author']."</a><br><br>";
                     }
                 ?>
+                <hr>
+                <a href="/blogs">View more blog posts</a>
+                <br>
+                <br>
+                <div class="topBarWithItemsThing">
+                    <a href="blogs">Blogs</a> &nbsp;<a href="groups">Groups</a> &nbsp;<a href="register">Register</a> &nbsp;<a href="login">Login</a>
+                </div>
                 <br>
                 THIS IS UNDER CONSTRUCTION!!
             </div>
@@ -50,7 +56,7 @@
                     //preparing a statement isn't necessary, you're not querying with php input
                     $result = $conn->query("SELECT id, username FROM `users`");
                     while($row = $result->fetch_assoc()) {
-                        echo "<a href='profile.php?id=" . $row['id'] . "'>" . $row['username'] . "</a><br>";
+                        echo "<a href='profile?id=" . $row['id'] . "'>" . $row['username'] . "</a><br>";
                     }
                 ?>
             </div>
