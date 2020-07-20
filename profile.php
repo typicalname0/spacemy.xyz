caf235b77d04f7cd8668506d0c62832c69e55bda
typicalname <46434915+typicalname0@users.noreply.github.com>
2020-07-19 20:47:05 -0700

    embed

--- profile.php
+++ profile.php
@@ -43,6 +43,9 @@
                 header('Location: ' . $_SERVER['HTTP_REFERER']);
             }
         ?>
+        <meta property="og:title" content="<?php echo $user; ?>"/>
+        <meta property="og:image" content="https://spacemy.xyz/pfp/<?php echo $pfp; ?>"/>
+        <meta property="og:description" content="<?php echo str_replace("<br>", PHP_EOL, $bio); ?>" />
         <style><?php echo $row['css']; ?></style>
     </head>
     <body>
