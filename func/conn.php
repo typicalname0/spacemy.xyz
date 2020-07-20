11dd8995696cdac046030afca43972e8fb1d2c4b
pizzaboxer <41478239+pizzaboxer@users.noreply.github.com>
2020-07-20 11:45:22 +0100

    add captcha to new blogs and groups, as well as site key definitions in conn
    reason i put the site key definitions in conn.php instead of settings.php is because conn.php is supposed to hold the stuff that isnt meant to be exposed

--- func/conn.php
+++ func/conn.php
@@ -1,3 +1,5 @@
 <?php
     $conn = mysqli_connect("localhost", "", "", "spacemy");
+    define("CAPTCHA_PRIVATEKEY", "[private key goes here]");
+	define("CAPTCHA_SITEKEY", "[site key goes here]");
 ?>
--- newgroup.php
+++ newgroup.php
@@ -1,40 +1,53 @@
 <?php
     require("func/conn.php");
     require("func/settings.php");
-    requireLogin();
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="css/header.css">
         <link rel="stylesheet" href="css/base.css">
+        <script src='https://www.google.com/recaptcha/api.js' async defer></script>
+        <script>function onLogin(token){ document.getElementById('submitform').submit(); }</script>
     </head>
     <body>
         <?php
             require("header.php");
+            if($_SERVER['REQUEST_METHOD'] == 'POST') 
+            {
+                if(!$_POST['groupname']){ $error = "you must specify a group title"; goto skip; }
+                if(!$_POST['desc']){ $error = "you must specify a group description"; goto skip; }
+                if(strlen($_POST['groupname']) > 32){ $error = "group title must be shorter than 32 characters"; goto skip; }
+                if(strlen($_POST['desc']) > 500){ $error = "group description must be shorter than 500 characters"; goto skip; }
+                if(!$_POST['g-recaptcha-response']){ $error = "captcha validation failed"; goto skip; }
+
+                $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.CAPTCHA_PRIVATEKEY.'&response='.$_POST['g-recaptcha-response']);
+                $responseData = json_decode($verifyResponse);
+
+                if(!$responseData->success) { $error = "captcha validation failed"; goto skip; }
+
+                $stmt = $conn->prepare("INSERT INTO `groups` (name, description, author, date) VALUES (?, ?, ?, now())");
+                $stmt->bind_param("sss", $name, $text, $_SESSION['user']);
+                $text = str_replace(PHP_EOL, "<br>", $_POST['desc']);
+                $name = htmlspecialchars($_POST['groupname']);
+                $stmt->execute();
+                $stmt->close();            
+
+                $stmt = $conn->prepare("UPDATE users SET currentgroup = ? WHERE username = ?");
+                $stmt->bind_param("ss", $groupname, $_SESSION['user']);
+                $groupname = htmlspecialchars($_POST['groupname']);
+                $stmt->execute();
+                $stmt->close();  
+                header("Location: groups.php");             
+            }
+            skip:
         ?>
         <div class="container">
-            <?php
-                if(@$_POST) {
-                    $stmt = $conn->prepare("INSERT INTO `groups` (name, description, author, date) VALUES (?, ?, ?, now())");
-                    $stmt->bind_param("sss", $name, $text, $_SESSION['user']);
-                    $text = str_replace(PHP_EOL, "<br>", $_POST['desc']);
-                    $name = htmlspecialchars($_POST['groupname']);
-                    $stmt->execute();
-                    $stmt->close();            
-
-                    $stmt = $conn->prepare("UPDATE users SET currentgroup = ? WHERE username = ?");
-                    $stmt->bind_param("ss", $groupname, $_SESSION['user']);
-                    $groupname = htmlspecialchars($_POST['groupname']);
-                    $stmt->execute();
-                    $stmt->close();  
-                    header("Location: groups.php");              
-                }
-            ?>
-            <form method="post" enctype="multipart/form-data">
-                <input required placeholder="Name" size="90" type="text" name="groupname"><br>
-				<textarea required rows="10" cols="68" placeholder="Description" name="desc"></textarea><br>
-				<input name="submit" type="submit" value="Create"> <small>max limit: 500 characters</small>
+            <?php if(isset($error)) { echo "<small style='color:red'>".$error."</small>"; } ?>
+            <form method="post" enctype="multipart/form-data" id="submitform">
+                <input required placeholder="Name [32 characters max]" size="90" type="text" name="groupname"><br>
+                <textarea required rows="10" cols="68" placeholder="Description [500 characters max]" name="desc"></textarea><br>
+                <input type="submit" value="Create" class="g-recaptcha" data-sitekey="<?php echo CAPTCHA_SITEKEY; ?>" data-callback="onLogin">
             </form>
         </div>
     </body>
