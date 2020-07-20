15a4bedbb8a74c1742a427367d35bb6a8469c048
ezist <maxwell1998r@gmail.com>
2020-07-19 20:11:13 -0500

    Add group shouts and revamp group design

--- viewgroup.php
+++ viewgroup.php
@@ -7,6 +7,10 @@
     <head>
         <link rel="stylesheet" href="css/header.css">
         <link rel="stylesheet" href="css/base.css">
+        <style type="text/css">
+            #left {float: left;width:59%;}
+            #right {float: right;width:39%;}
+        </style>
         <?php
             $stmt = $conn->prepare("SELECT * FROM `groups` WHERE id = ?");
             $stmt->bind_param("i", $_GET['id']);
@@ -18,6 +22,7 @@
                 $desc = $row['description'];
                 $author = $row['author'];
                 $date = $row['date'];
+                $shout = $row['shout'];
             }
 
             if(@$_POST["comment"]) {
@@ -29,7 +34,12 @@
                 $stmt->execute();
             
                 $stmt->close();
-                
+            }
+            if(@$_POST['shout']) {
+                $stmt = $conn->prepare("UPDATE `groups` SET shout = ? WHERE id = ?");
+                $stmt->bind_param("si", $_POST['shout'], $_GET['id']);
+                $stmt->execute();
+                $stmt->close();
             }
         ?>
     </head>
@@ -38,55 +48,71 @@
             require("header.php");
         ?>
         <div class="container">
-            <h1><?php echo $name; ?></h1>
-            <?php
-                if (isset($_SESSION['user'])) {
-                    $stmt = $conn->prepare("SELECT * FROM `users` WHERE username = ?");
-                    $stmt->bind_param("s", $_SESSION['user']);
+            <h1><?php echo $name;?></h1>
+            <pre><?php echo $desc;?></pre>
+            <hr/>
+            <div id="left">
+                <b id="shout"><?php echo $shout;?></b><br/><br/>
+                <div class="info">
+                    <center>Comments</center>
+                </div>
+                <form method="post" enctype="multipart/form-data">
+                    <textarea required rows="5" cols="44" placeholder="Comment" name="comment"></textarea><br>
+                    <input name="submit" type="submit" value="Post"> <small>max limit: 500 characters</small>
+                </form>
+                <br>
+                <?php
+                    $stmt = $conn->prepare("SELECT * FROM `groupcomments` WHERE toid = ?");
+                    $stmt->bind_param("s", $_GET['id']);
                     $stmt->execute();
                     $result = $stmt->get_result();
-                    while ($row = $result->fetch_assoc()) {
-                        if($row['currentgroup'] === $name) {
-                            echo "<a href='leavegroup.php'><button>Leave Group</button></a><br/><br/>";
+                    
+                    while($row = $result->fetch_assoc()) {
+                        echo "<div class='commentRight'>";
+                        echo "  <small>" . $row['date'] . "</small><br>" . $row['text'];
+                        echo "  <a style='float: right;' href='profile.php?id=" . getID($row['author'], $conn) . "'>" . $row['author'] . "</a> <br>";
+                        echo "  <img class='commentPictures' style='float: right;' width='80px;'src='pfp/" . getPFP($row['author'], $conn) . "'><br><br><br><br><br>";
+                        echo "</div>";
+                    }
+                 ?>
+            </div>
+            <div id="right">
+                <?php
+                    echo "Owner: <a href='profile.php?id=" . getID($author, $conn) . "'>" . $author . "</a><br/><br/>";
+                    echo "Members:<br/>";
+                    $stmt = $conn->prepare("SELECT * FROM `users` WHERE currentgroup = ?");
+                    $stmt->bind_param("s", $name);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+
+                    while($row = $result->fetch_assoc()) {
+                        echo "<a href='profile.php?id=" . $row['id'] . "'>" . $row['username'] . "</a><br/>";
+                    }
+                ?><br/>
+                <?php
+                    if (isset($_SESSION['user'])) {
+                        $stmt = $conn->prepare("SELECT * FROM `users` WHERE username = ?");
+                        $stmt->bind_param("s", $_SESSION['user']);
+                        $stmt->execute();
+                        $result = $stmt->get_result();
+                        while ($row = $result->fetch_assoc()) {
+                            if($row['currentgroup'] === $name) {
+                                echo "<a href='leavegroup.php'><button>Leave Group</button></a>";
+                            }
+                            if($author === $_SESSION['user']) {?>
+                                <br/><br/>
+                                <form method="post" enctype="multipart/form-data">
+                                    <textarea required rows="5" cols="20" placeholder="Shout" name="shout"></textarea><br/>
+                                    <input name="submit" type="submit" value="Change Group Shout">
+                                    <small>max limit: 50 characters</small>
+                                </form>
+                            <?php
+                            }
+                            if ($row['currentgroup'] === $name || $author === $_SESSION['user']) {echo "<br/>";}
                         }
                     }
-                }
-            ?>
-            <?php
-                echo "Owner: <a href='profile.php?id=" . getID($author, $conn) . "'>" . $author . "</a><br/><br/>";
-                echo "Members:<br/>";
-                $stmt = $conn->prepare("SELECT * FROM `users` WHERE currentgroup = ?");
-                $stmt->bind_param("s", $name);
-                $stmt->execute();
-                $result = $stmt->get_result();
-
-                while($row = $result->fetch_assoc()) {
-                    echo "<a href='profile.php?id=" . $row['id'] . "'>" . $row['username'] . "</a><br/>";
-                }
-            ?>
-            <pre><?php echo $desc;?></pre>
-            <div class="info">
-                <center>Comments</center>
+                ?>
             </div>
-            <form method="post" enctype="multipart/form-data">
-				<textarea required rows="5" cols="80" placeholder="Comment" name="comment"></textarea><br>
-				<input name="submit" type="submit" value="Post"> <small>max limit: 500 characters</small>
-            </form>
-            <br><hr>
-            <?php
-                $stmt = $conn->prepare("SELECT * FROM `groupcomments` WHERE toid = ?");
-                $stmt->bind_param("s", $_GET['id']);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                
-                while($row = $result->fetch_assoc()) {
-                    echo "<div class='commentRight'>";
-                    echo "  <small>" . $row['date'] . "</small><br>" . $row['text'];
-                    echo "  <a style='float: right;' href='profile.php?id=" . getID($row['author'], $conn) . "'>" . $row['author'] . "</a> <br>";
-                    echo "  <img class='commentPictures' style='float: right;' width='80px;'src='pfp/" . getPFP($row['author'], $conn) . "'><br><br><br><br><br>";
-                    echo "</div>";
-                }
-            ?>
         </div>
     </body>
 </html>
