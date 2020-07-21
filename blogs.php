cc7ff5d7ab8034635e755e178a404dacc681fe53
ezist <maxwell1998r@gmail.com>
2020-07-21 00:06:15 -0500

    Fix blogs pagination for fucks sake

--- blogs.php
+++ blogs.php
@@ -25,21 +25,25 @@
                 $stmt1 = $conn->prepare("SELECT COUNT(*) FROM `blogs`");
                 $stmt1->execute();
                 $results = $stmt1->get_result();
-                $pgs = $results->fetch_assoc()["COUNT(*)"];
+                $pgs = ceil($results->fetch_assoc()["COUNT(*)"] / 20);
                 $pgOffset = 0;
                 if (isset($_GET["pg"])) {
-                    $pgOffset = -20 + (intval($_GET["pg"])*20);
+                    $pgOffset = -20 + (intval($_GET["pg"]) * 20);
                 }
                 $stmt = $conn->prepare("SELECT * FROM `blogs` LIMIT 20 OFFSET $pgOffset");
                 $stmt->execute();
                 $result = $stmt->get_result();
-                for ($i=1; $i<$pgs or $i<9; $i++) {
+                for ($i=1; $i < $pgs && $i < 9; $i++) {
                     echo "<a href='blogs.php?pg=$i'>$i</a> &bull;";
                 }
-                if ($i !== $pgs){
-                    echo "<a href='blogs.php?pg=$pgs'> ..$pgs</a>";
+                if ($i !== $pgs) {
+                    if ($i === 1) {
+                        echo "<a href='blogs.php?pg=$pgs'>$pgs</a>";
+                    } else {
+                        echo "<a href='blogs.php?pg=$pgs'>..$pgs</a>";
+                    }
                 }
-                echo "<br>";
+                echo "<br><br>";
                 while($row = $result->fetch_assoc()) {
                     echo "<b>" . $row['title'] . "</b> - " . $row['author'] . "@" . $row['date'] . " <a style='float: right;' href='viewblog.php?id=" . $row['id'] . "'><small>[view]</small></a><hr>";
                 }
