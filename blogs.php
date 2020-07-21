3080e5713725522b4efe1ceff268a0b96ed7c1c1
pizzaboxer <41478239+pizzaboxer@users.noreply.github.com>
2020-07-21 11:26:32 +0100

    redo the awful paginationt that kermeet did

--- blogs.php
+++ blogs.php
@@ -11,10 +11,31 @@
     <body>
         <?php
             require("header.php");
+                if(isset($_GET['page'])){ $page = $_GET['page']; }
+                else{ $page = 1; }
+                if(!filter_var($page, FILTER_VALIDATE_INT)){ $page = 1; }
+
+                $stmt = $conn->query("SELECT id FROM `blogs`");
+                $numblogs = $stmt->num_rows;
+                $pages = ceil($numblogs/20);
+                $offset = ($page - 1)*20;
+
+                $stmt = $conn->prepare("SELECT * FROM `blogs` LIMIT 20 OFFSET ?");
+                $stmt->bind_param("i", $offset);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                $stmt->close();
         ?>
         <div class="container">
             <h1>Blogs [wip]</h1>
-            <form action="/search.php" method="get" class="search">
+            <div style="text-align:center;">
+                <a <?php if($page != 1){ echo 'href="?page='.($page-1).'"'; } ?>>&lt;&lt; Back</a>
+                <?php echo "[ Page $page out of $pages ]"; ?>
+                <a <?php if ($page < $pages) { echo 'href="?page='.($page+1).'"'; } ?>>Next &gt;&gt;</a>
+            </div>
+            <br>
+            <form action="/search.php" method="post" class="search">
                 <input placeholder="Search for blogs..." size="59" type="text" name="query">
                 <input type="hidden" name="queryfor" value="Blogs">
                 <input type="submit" value="Search">
@@ -22,32 +43,18 @@
             </form>
             <hr>
             <?php
-                $stmt1 = $conn->prepare("SELECT COUNT(*) FROM `blogs`");
-                $stmt1->execute();
-                $results = $stmt1->get_result();
-                $pgs = ceil($results->fetch_assoc()["COUNT(*)"] / 20);
-                $pgOffset = 0;
-                if (isset($_GET["pg"])) {
-                    $pgOffset = -20 + (intval($_GET["pg"]) * 20);
-                }
-                $stmt = $conn->prepare("SELECT * FROM `blogs` LIMIT 20 OFFSET $pgOffset");
-                $stmt->execute();
-                $result = $stmt->get_result();
-                for ($i=1; $i < $pgs && $i < 9; $i++) {
-                    echo "<a href='blogs.php?pg=$i'>$i</a> &bull;";
-                }
-                if ($i !== $pgs) {
-                    if ($i === 1) {
-                        echo "<a href='blogs.php?pg=$pgs'>$pgs</a>";
-                    } else {
-                        echo "<a href='blogs.php?pg=$pgs'>..$pgs</a>";
-                    }
-                }
-                echo "<br><br>";
-                while($row = $result->fetch_assoc()) {
+                while($row = $result->fetch_assoc()) 
+                {
                     echo "<b>" . $row['title'] . "</b> - " . $row['author'] . "@" . $row['date'] . " <a style='float: right;' href='viewblog.php?id=" . $row['id'] . "'><small>[view]</small></a><hr>";
                 }
+                if(!mysqli_num_rows($result)){ echo "<b>No blogs found</b>"; }
             ?>
+            <br>
+            <div style="text-align:center;">
+                <a <?php if($page != 1){ echo 'href="?page='.($page-1).'"'; } ?>>&lt;&lt; Back</a>
+                <?php echo "[ Page $page out of $pages ]"; ?>
+                <a <?php if ($page < $pages) { echo 'href="?page='.($page+1).'"'; } ?>>Next &gt;&gt;</a>
+            </div>
         </div>
     </body>
 </html>
