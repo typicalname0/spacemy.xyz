251c84c4a02a4ef0fe03ca44cbcac402c2114e1e
lightbulblighter <59720715+lighterlightbulb@users.noreply.github.com>
2020-07-29 03:27:03 -0500

    Code cleanup
    Improves game FPS by 600%!

--- func/conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "", "", "spacemy");
-    define("CAPTCHA_PRIVATEKEY", "[private key goes here]");
-	define("CAPTCHA_SITEKEY", "[site key goes here]");
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require("func/settings.php");
-if(isset($_SESSION['user'])) 
-{ 
-	$_SESSION = []; 
-	session_destroy();
-}
-header("Location: /landing.php");
