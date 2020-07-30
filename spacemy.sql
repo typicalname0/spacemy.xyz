f932b6af21f4ffe30b17a2e7efc90296509bc138
typicalname <46434915+typicalname0@users.noreply.github.com>
2020-07-29 18:49:33 -0700

    new sql

--- spacemy.sql
+++ spacemy.sql
@@ -3,11 +3,35 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `spacemy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogcomments`
+--
+
+CREATE TABLE `blogcomments` (
+  `id` int NOT NULL,
+  `toid` int NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `text` varchar(500) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogs`
+--
 
 CREATE TABLE `blogs` (
   `id` int NOT NULL,
@@ -17,6 +41,12 @@ CREATE TABLE `blogs` (
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
 CREATE TABLE `comments` (
   `id` int NOT NULL,
   `toid` int NOT NULL,
@@ -25,12 +55,24 @@ CREATE TABLE `comments` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
 CREATE TABLE `friends` (
   `sender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `reciever` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'PENDING',
   `id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groupcomments`
+--
 
 CREATE TABLE `groupcomments` (
   `id` int NOT NULL,
@@ -40,13 +82,39 @@ CREATE TABLE `groupcomments` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
 CREATE TABLE `groups` (
   `name` varchar(255) NOT NULL,
   `description` varchar(500) NOT NULL,
   `author` varchar(255) NOT NULL,
   `date` datetime NOT NULL,
-  `id` int NOT NULL
+  `id` int NOT NULL,
+  `shout` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invites`
+--
+
+CREATE TABLE `invites` (
+  `invitekey` varchar(255) NOT NULL,
+  `createdBy` int NOT NULL,
+  `usedBy` int DEFAULT NULL,
+  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
@@ -58,46 +126,109 @@ CREATE TABLE `users` (
   `css` varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `music` varchar(255) NOT NULL DEFAULT 'default.mp3',
   `pfp` varchar(255) NOT NULL DEFAULT 'default.jpg',
-  `currentgroup` int NOT NULL,
+  `currentgroup` int NOT NULL DEFAULT '1',
   `id` int NOT NULL,
-  `status` varchar(255) NOT NULL DEFAULT ''
+  `status` varchar(255) NOT NULL DEFAULT '',
+  `ranks` varchar(255) NOT NULL DEFAULT '',
+  `lastactive` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nickname` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `blogcomments`
+--
+ALTER TABLE `blogcomments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `blogs`
+--
 ALTER TABLE `blogs`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `comments`
+--
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `friends`
+--
 ALTER TABLE `friends`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `groupcomments`
+--
 ALTER TABLE `groupcomments`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `groups`
+--
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `invites`
+--
+ALTER TABLE `invites`
+  ADD KEY `invitekey` (`invitekey`);
+
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `blogcomments`
+--
+ALTER TABLE `blogcomments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `blogs`
+--
 ALTER TABLE `blogs`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `comments`
+--
 ALTER TABLE `comments`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `friends`
+--
 ALTER TABLE `friends`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `groupcomments`
+--
 ALTER TABLE `groupcomments`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `groups`
+--
 ALTER TABLE `groups`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
